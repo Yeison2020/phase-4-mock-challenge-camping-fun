483b9484bcc270f77c5e5267a885eefff1d87f28
Yeison <enmanuelquiterios@gmail.com>
2021-11-30 15:14:34 -0500

    Reviewing Lab

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,15 +1,17 @@
 class CampersController < ApplicationController
-
+    # The include just clean the serializer and does not include the camper serializer
     def index 
-        campers = Camper.all
-        render json: campers, status: :ok
+        #No using the serializer
+        render json: Camper.all, include: [:id, :name, :age]
+
     end
 
-
+    # While here the serializer apply and the index, because I added the the include did not  use the serializer.
     def show 
         camper = Camper.find_by(id: params[:id])
         if  camper
-            render json: camper, serializer:  CamperActivitiesSerializer 
+            # Using serializer
+            render json: camper
         else 
             render json: {error: "Camper not found"}, status: :not_found
         end
@@ -18,11 +20,14 @@ class CampersController < ApplicationController
 
     def create 
         new_camper = Camper.create(camper_params)
-        if new_camper 
-            render json: new_camper, status: :created
+        if new_camper.valid?
+            # I added the inluclude to get new camper without Its activities
+            render json: new_camper,include: [:id, :name, :age], status: :created
         else  
+            # I  can add the number of the status instead
+            # Example status: 422 better unprocessable_entity
             render json: {
-                errors: ["validation errors"]
+                errors: new_camper.errors.full_massages
               }, status: :unprocessable_entity
         end
 
